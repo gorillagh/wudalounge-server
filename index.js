6fd289a24f05cf409b711884bc17f696a5183e1f
Albert <ron468896@gmail.com>
2023-03-20 13:13:17 +0000

    fix cors problem in websocket

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = app.listen(port, () => {
 //Socket.IO setup
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://www.wudalounge.com/admin", //Allow requests from this origin
+    origin: "https://www.wudalounge.com", //Allow requests from this origin
     methods: ["GET", "POST"],
   },
 });
